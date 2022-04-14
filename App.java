b697b8f7b68d680f8ecafbd215a7090e25b6c651
geddmatias <100250540+geddmatias@users.noreply.github.com>
2022-04-14 22:23:40 +0800

    Add files via upload

---
+++ App.java
@@ -0,0 +1,56 @@
+public class App {
+    /**
+     * modifying methods to sum()
+     */
+    private static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    private static int sum(int num1, int num2, int num3) {
+        return num1 + num2 + num3;
+    }
+    /**
+     * method to return greater int value
+     */
+    public static int greaterValue(int Num1, int Num2) {
+
+        if (Num1 > Num2) {
+            return Num1;
+        } else {
+            return Num2;
+        }
+    }
+    /**
+     * method to return greater double value
+     */
+    public static double greaterValue(double Num1, double Num2) {
+
+        if (Num1 > Num2) {
+            return Num1;
+        } else {
+            return Num2;
+        }
+    }
+
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        /**
+         * using overloading
+         */
+
+        System.out.println("Sum of two numbers 5 and 10: " + sum(5, 10));
+        System.out.println("Sum of three numbers 5, 10 , 15: " + sum(5, 10, 15));
+        System.out.println("------------------------------");
+        /**
+         * Call the greaterValue method with integer parameters
+         */
+        System.out.println("The greater value between 45 and 100 is " + greaterValue(45,100));
+
+        /**
+         * Call the greaterValue method with double parameters
+         */
+        System.out.println("The greater value between 3.14 and 9.8 is " + greaterValue(3.14,9.8));
+
+    }
+}
